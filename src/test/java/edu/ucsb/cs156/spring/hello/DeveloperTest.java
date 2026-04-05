68f91a1fe0182d11ef8f70708b7758e313383018
Ivy Holiday <iholiday@ucsb.edu>
2026-04-04 17:23:10 -0700

    Added getGithubId test

--- src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
@@ -24,6 +24,11 @@ public class DeveloperTest {
     public void getName_returns_correct_name() {
         assertEquals("Ivy", Developer.getName());
     }
+    
+    @Test
+    public void getGithubId_returns_correct_id() {
+        assertEquals("ikeacat2", Developer.getGithubId());
+    }
 
     // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
     // 100% mutation coverage (all mutants timed out or killed)
